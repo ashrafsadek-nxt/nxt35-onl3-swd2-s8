0db14cf61b90808927e453942e5454f02d9b399e
shahd421 <shahdelghandour24@gmail.com>
2025-08-12 15:56:31 +0300

    Add files via upload

---
+++ shahd_magdy/first js task/index.js
@@ -0,0 +1,54 @@
+var myname="shahd"
+console.log(typeof(myname));
+console.log(myname);
+myname="magdy";
+console.log(myname);
+
+var u=123;
+console.log(u);
+console.log(typeof(u));
+
+u="123";
+console.log(typeof(u));
+console.log(u+10);
+console.log(parseInt(u)+10);
+
+var w=['ahmed',122,true];
+console.log(w);
+console.log(w[2]);
+console.log(typeof(w[2]));
+
+var info = {
+    name: "shahod",
+    age: 20
+}
+console.log(info);
+console.log(typeof(info));
+console.log(info.age);
+console.log(typeof(info.age));
+
+var sum = total => {} 
+console.log(sum);
+console.log(typeof(sum));
+
+console.log(typeof(parseFloat("125")));
+console.log(parseFloat("125"));
+console.log(typeof(parseFloat(123.258)));
+console.log(parseFloat(123.258));
+console.log(typeof(parseFloat(0.217)));
+console.log(parseFloat(0.98));
+
+
+var p = "200";
+console.log(p*5);
+console.log(typeof(p*5));
+var p= "400";
+console.log(p/5);
+console.log(typeof(p/5));
+
+const m=document.getElementById('first').value;
+console.log(m)
+
+const n=document.getElementById('second');
+n.value=40;
+console.log(n);
