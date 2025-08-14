19256553b6b134e93880bfca5516a543c7e3000e
N3ma-Rahim <neamaarahim05@gmail.com>
2025-08-14 18:17:23 +0300

    Add files via upload

---
+++ Neama Abdelrahim/Week 6/assets/js/script.js
@@ -0,0 +1,37 @@
+function showStudentsGrades() {
+    let students = {
+        "Ahmed": 95,
+        "Sara": 82,
+        "Omar": 70,
+        "Laila": 62,
+        "Mona": 40
+    }
+    let container = document.getElementById("studentsContainer");
+    container.innerHTML = "";
+    for (let name in students) {
+        let score = students[name]
+        let grade = getGrade(score)
+        let studentDiv = document.createElement("div");
+        studentDiv.className = `grade-${grade}`;
+        studentDiv.textContent = `${name}-score:${score}-grade:${grade}`;
+        container.appendChild(studentDiv);
+    }
+}
+function getGrade(score) {
+    if (score >= 90) return "A";
+    else if (score >= 80) return "B";
+    else if (score >= 70) return "C";
+    else if (score >= 60) return "D";
+    else return "F";
+}
+function checkGrade() {
+    let score = document.getElementById("scoreInput").value;
+    if (score === "") {
+        document.getElementById("gradeResult").textContent = "Please enter a score!";
+        return;
+    }
+    let grade = getGrade(Number(score));
+    document.getElementById("gradeResult").textContent = `Your grade is: ${grade}`;
+}
+document.getElementById("checkBtn").addEventListener("click", checkGrade);
+showStudentsGrades();
