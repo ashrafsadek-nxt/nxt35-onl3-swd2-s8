6cf2af213fe5321c4055fe6cc8a2a99dc978c946
mohamedemad-basha <144592551+mohamedemad-basha@users.noreply.github.com>
2025-09-06 15:07:03 +0300

    upload Events with typescript task

---
+++ Mohamed Basha/Session Activities & Tasks/Events with typescript/ap.ts
@@ -0,0 +1,29 @@
+let x: number = 1000;
+let y: string = "ahmed";
+let f: number = 10.55;
+let r: boolean = true;
+let address: { street: string; city: string } = {
+  street: "nasr",
+  city: "cairo",
+};
+let salary: { amount: number; currency: string } = {
+  amount: 12345,
+  currency: "egp",
+};
+let children: string[] = ["ahmed", "aly"];
+let data: number[] = [2, 5, 8];
+let info: any = "123";
+function showData() {
+  const outputDv = document.getElementById("output");
+  outputDv!.innerHTML = `
+        <p>${x}</p>
+        <p>${y}</p>
+        <p>${f}</p>
+        <p>${r}</p>
+        <p>${address.city}</p>
+        <p>${children[0]} and ${children[1]}</p>
+    `;
+}
+
+const btn = document.querySelector("#btn");
+btn!.addEventListener("click", showData);
