e8bd868060b15617dbfbbc5f4f649bbfcbd26577
Youssef Emad <youssef.dev.84@gmail.com>
2025-09-24 07:22:56 +0300

    Add files via upload

---
+++ Youssef Emad/week-14-friday/src/layouts/Main/Main.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./main.css";
+
+export default function Main() {
+  return (
+    <main className="main">
+      <h2>Welcome to My Website</h2>
+      <p>This is the main content area. Add whatever you want here.</p>
+    </main>
+  );
+}
