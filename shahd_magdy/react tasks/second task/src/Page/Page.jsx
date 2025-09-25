ae7eea1a42879f95d89955359e4a9ad41a4985c0
shahd421 <shahdelghandour24@gmail.com>
2025-09-26 01:49:41 +0300

    Add files via upload

---
+++ shahd_magdy/react tasks/second task/src/Page/Page.jsx
@@ -0,0 +1,32 @@
+import flower from "../assets/images/flower.jpg"
+import "./Page.css"
+function Page()
+{
+   return(
+    <div className="page">
+    <div className="nav">
+        <ul>
+            <li>
+                <a href="">prouduct</a>
+            </li>
+            <li>
+                <a href="">shosho</a>
+            </li>
+            <li>
+                <a href="">menna</a>
+            </li>
+        </ul>
+
+    </div>
+    <div className="cards">
+        <div className="card"><img src={flower} alt=""  className="logo"/></div>
+        <div className="card"><img src={flower} alt=""  className="logo"/></div>
+        <div className="card"><img src={flower} alt="" className="logo" /></div>
+        <div className="card"><img src={flower} alt=""  className="logo"/></div>
+
+    </div>
+    </div>
+
+   )
+}
+export default Page;
