e99622fdc344daa2bb8038b2502d5fc712d21c19
Omar Yasser <omar.yasser1482@gmail.com>
2025-09-30 00:03:52 +0300

    Add files via upload

---
+++ Omar Yasser/react tasks/second react/my-react-app/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Footer from './footer.jsx'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+      <p>Some content here...</p>
+
+      {/* Footer sticks to bottom */}
+      <Footer />
+    </div>
+  )
+}
+
+export default App
