0cb89c414f6385c875d9726f2f9fabf444c2785f
Omar Yasser <omar.yasser1482@gmail.com>
2025-09-30 00:01:13 +0300

    Add files via upload

---
+++ Omar Yasser/react tasks/usestateexample/src/footer.jsx
@@ -0,0 +1,11 @@
+import "./footer.css";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Labore perferendis cum recusandae, incidunt quaerat in est facere quam nostrum, debitis fugiat repellat culpa laboriosam ullam distinctio, esse architecto aliquam voluptatibus?</p>
+    </footer>
+  );
+}
+
+export default Footer;
