7ccbe8040d4181233a9785e3fc067fed7ec5c75c
Marina Ashraf <marinaashraf565@gmail.com>
2025-10-08 10:50:52 +0300

    Add files via upload

---
+++ Marina Ashraf Churchill/js/Show users Task/first.js
@@ -0,0 +1,22 @@
+const fiveB =document.getElementById("fiveB");
+
+fiveB.addEventListener('click',()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response=>{
+        if(!response.ok){
+            throw new Error ("error occure")
+        }
+        return response.json();
+    }).then (data=>{
+        const list =document.getElementById("lst")
+        list.innerHTML='';
+      data.forEach(user=>{
+        const li =document.createElement('li');
+        li.textContent=`${user.name} -------${user.email}`;
+list.appendChild(li)
+      });
+            
+        }).catch(error=>{
+            alert('loading failed')
+        })
+    })
