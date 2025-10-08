a24dd05a47d969861ebb1b97b40ba699e6cd36eb
Marina Ashraf <marinaashraf565@gmail.com>
2025-10-09 00:14:04 +0300

    Create code.js

---
+++ Marina Ashraf Churchill/js/Lecture Code/code.js
@@ -0,0 +1,28 @@
+
+
+document.getElementById("first").innerHTML +="<h1>hello</h1>";
+document.getElementById("firstp").innerText="bayyye";
+document.getElementById("inputf").value=40;
+
+
+
+function marina(){
+    var marina=10;
+    if ( marina==10){
+        var x=20
+        document.write("marina=10 "+ x)
+    }
+    else{
+         document.write("marina=0")
+    }
+    console.log(marina)
+     console.log(x)
+}
+marina()
+
+// var x =10;
+// console.log(x)
+// var x="marina"; //redeclaration
+// console.log(x);
+// x=12.5;//re-assign
+// console.log(x);
