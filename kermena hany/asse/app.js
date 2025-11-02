63cd2d2a52753235a4031c54e80a4007ad784f3c
Kermena hany <krkrhany124@gmail.com>
2025-11-02 20:24:14 +0200

    asse

---
+++ kermena hany/asse/app.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let x = 12;
+console.log(x);
+var n = 1000;
+console.log(n);
+var y = "ahmed";
+console.log(y);
+var xy = 103400;
+console.log(xy);
+var yz = "ahmeasdf hhjjhjd";
+console.log(yz);
+//# sourceMappingURL=app.js.map
