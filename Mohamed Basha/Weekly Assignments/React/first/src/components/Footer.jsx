39c60e9697d57b3aab81cf389e95539803498033
mohamedemad-basha <144592551+mohamedemad-basha@users.noreply.github.com>
2025-11-15 04:43:26 +0200

    Upload React First task componants

---
+++ Mohamed Basha/Weekly Assignments/React/first/src/components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer
+      style={{
+        background: "#333",
+        color: "#fff",
+        padding: "10px",
+        textAlign: "center",
+      }}
+    >
+      &copy; 2025 My React App
+    </footer>
+  );
+}
