4a3448a2b40ffdbbd4418a419392dfce4bba9448
nouraemad1 <nouraemad182@gmail.com>
2025-11-19 14:35:09 +0200

    Add files via upload

---
+++ Noura Emad/react_tasks/first_task/App.jsx
@@ -0,0 +1,22 @@
+
+import Navbar from "./layout/navbar/navbar"
+import Sidebar from "./sidebar/Sidebar"
+import "./App.css"
+import Content from "./content/Content"
+import Footer from  "./Footer/Footer"
+function App() {
+  
+  return (
+    <div className="app">
+      <Navbar/>
+        <div className="main" >
+         
+      </div>
+      
+      <Footer></Footer>
+      
+    </div>
+  )
+}
+
+export default App
