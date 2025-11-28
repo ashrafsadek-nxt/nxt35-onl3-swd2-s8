ea9ec64e3565537f8a6b07c99534f81567d041b8
aolmaking <ahmedsalahelsayed94@gmail.com>
2025-11-28 21:20:32 +0200

    Add files via upload

---
+++ ahmed-salah/assingment 15/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+// import Footer from "./footer/Footer.jsx"
+import CartApp from './CartApp/CartApp.jsx'
+function App() {
+  return (
+    <>
+    <CartApp/>
+    </>
+  )
+}
+
+export default App
